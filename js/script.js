6c9e6fbd8e64eafd8842e80343b001194705183e
Молотков Дмитрий <d.molotkof@gmail.com>
2023-08-11 22:06:52 +0300

    change

--- js/script.js
+++ js/script.js
@@ -13,75 +13,44 @@ if (window.innerWidth < 768) {
 // Получение ссылки на кнопку с классом "brands__click_show"
 const btn = document.querySelector(".brands__click_show");
 // Получение массива карточек с классом "swiper-slide"
-const cards = Array.from(document.querySelectorAll(".swiper-slide"));
-
+const cards = document.querySelectorAll(".swiper-slide");
+console.log(cards);
 // Добавление обработчиков события "resize" и "click"
 window.addEventListener("resize", handleResize);
 btn.addEventListener("click", handleClick);
 
 // Функция обработчика события "resize"
 function handleResize() {
-  // Проверка ширины окна и вызов соответствующей функции
-  if (window.innerWidth > 767 && window.innerWidth < 1120) {
-    response1();
-  }
-  if (window.innerWidth > 1119) {
-    response2();
-  }
-}
-
-// Функция для обработки первого условия ширины окна
-function response1() {
-  // Применение класса "hidden" ко всем элементам массива карточек
   cards.forEach((item, index) => {
-    item.classList.add("hidden");
-    // Удаление класса "hidden" у элементов с индексом меньше 6
-    if (index < 6) {
-      item.classList.remove("hidden");
-    }
-  });
-}
-
-// Функция для обработки второго условия ширины окна
-function response2() {
-  // Применение класса "hidden" ко всем элементам массива карточек
-  cards.forEach((item, index) => {
-    item.classList.add("hidden");
-    // Удаление класса "hidden" у элементов с индексом меньше 8
-    if (index < 8) {
-      item.classList.remove("hidden");
+    item.classList.remove("hidden");
+    if (window.innerWidth > 767 && window.innerWidth < 1120 && index >= 6) {
+      item.classList.add("hidden");
+    } else if (window.innerWidth > 1119 && index >= 8) {
+      item.classList.add("hidden");
     }
   });
 }
 
 // Функция обработчика события "click" на кнопке
 function handleClick() {
-  // Проверка ширины окна и скрытие/отображение элементов
-  if (window.innerWidth > 1119) {
-    cards.forEach((item, index) => {
-      // Скрытие/отображение элементов с индексом больше или равным 8
-      if (index >= 8) {
-        item.classList.toggle("hidden");
-        btn.classList.toggle('brands__click_hide');
-      }
-    });
-  }
-  if (window.innerWidth > 767 && window.innerWidth < 1120) {
-    cards.forEach((item, index) => {
-      // Скрытие/отображение элементов с индексом больше или равным 6
-      if (index >= 6) {
-        item.classList.toggle("hidden");
-        btn.classList.toggle('brands__click_hide');
-      }
-    });
-  }
-
-  // Изменение текста кнопки в зависимости от текущего состояния
-  if (btn.textContent === "Показать все") {
-    btn.textContent = 'Скрыть';
-  } else {
-    btn.textContent = 'Показать все';
-  }
+  cards.forEach((item, index) => {
+    if (window.innerWidth > 1119 && index >= 8) {
+      item.classList.toggle("hidden");
+      btn.classList.toggle("brands__click_hide");
+    } else if (
+      window.innerWidth > 767 &&
+      window.innerWidth < 1120 &&
+      index >= 6
+    ) {
+      item.classList.toggle("hidden");
+      btn.classList.toggle("brands__click_hide");
+    }
+    if (btn.textContent === "Показать все") {
+      btn.textContent = "Скрыть";
+    } else {
+      btn.textContent = "Показать все";
+    }
+  });
 }
 
 // Вызов функций при загрузке страницы
